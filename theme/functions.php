742e7606fa3b1be772ba0a07d7d2b46f51e9f0f3
jabber <2898117012@qq.com>
2022-07-09 10:57:29 +0800

    init

--- theme/functions.php
+++ theme/functions.php
@@ -2,7 +2,7 @@
 if ( ! function_exists( 'venus_support' ) ) {
 	function venus_support() {
 		add_theme_support( 'automatic-feed-links' );
-		
+
 	}
 }
 
@@ -27,6 +27,7 @@ if ( ! function_exists( 'venus_scripts' ) ) {
 		$version_string = is_string( $theme_version ) ? $theme_version : false;
 		wp_register_script( 'venus-script', get_template_directory_uri() . '/assets/js/app.js', array(), $version_string );
 		wp_enqueue_script( 'venus-script' );
+		wp_script_add_data( 'venus-script', 'async', true );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'venus_scripts' );
