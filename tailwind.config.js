af3a79bdd719df692341cad4678c51ef94c18e15
jabber <2898117012@qq.com>
2022-07-12 08:19:29 +0800

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,30 @@ module.exports = {
 		'./theme/**/*.php',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					50: '#fef2f3',
+					100: '#fde6e7',
+					200: '#f9bfc4',
+					300: '#f599a0',
+					400: '#ee4d59',
+					500: '#e60012',
+					600: '#cf0010',
+					700: '#ad000e',
+					800: '#8a000b',
+					900: '#710009'
+				}
+			},
+			fontFamily: {
+				sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	experimental: {
 		optimizeUniversalDefaults: true,
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/forms'),
+	],
 }
