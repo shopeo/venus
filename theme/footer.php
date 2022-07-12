6c9e4a074f7abb9c7e0203f713bf94db190895ee
jabber <2898117012@qq.com>
2022-07-12 12:54:30 +0800

    init

--- theme/footer.php
+++ theme/footer.php
@@ -1,5 +1,12 @@
-<footer id="site-footer">
-
+<footer id="site-footer" class="bg-neutral-700 text-white">
+	<div class="max-w-8xl p-4 mx-auto">
+		Hello World
+	</div>
+	<div class="max-w-8xl p-4 mx-auto border-t border-neutral-500">
+		<div class="footer-copyright">
+			&copy; <?php echo date_i18n( _x( 'Y', 'copyright date format', 'venus' ) ); ?>
+			<a class="uppercase" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></div>
+	</div>
 </footer>
 <?php wp_footer(); ?>
 </body>
