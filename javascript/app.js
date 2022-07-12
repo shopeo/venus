924d9a01668c1da95b7579cb7d927520e43dce34
jabber <2898117012@qq.com>
2022-07-12 11:07:01 +0800

    init

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,23 @@
+import Alpine from "alpinejs";
+import mask from "@alpinejs/mask";
+import intersect from "@alpinejs/intersect";
+import persist from "@alpinejs/persist";
+import focus from "@alpinejs/focus";
+import collapse from "@alpinejs/collapse";
+import morph from "@alpinejs/morph";
+import Clipboard from "@ryangjchandler/alpine-clipboard";
+
+Alpine.plugin(mask);
+Alpine.plugin(intersect);
+Alpine.plugin(persist);
+Alpine.plugin(focus);
+Alpine.plugin(collapse);
+Alpine.plugin(morph);
+Alpine.plugin(Clipboard);
+
+window.Alpine = Alpine;
+Alpine.start();
+
 (function ($) {
 
 })(jQuery);
