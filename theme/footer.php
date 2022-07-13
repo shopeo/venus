d8a8bd7c6d121aae7ddf67dff67d610b5b032365
jabber <2898117012@qq.com>
2022-07-13 12:21:38 +0800

    init

--- theme/footer.php
+++ theme/footer.php
@@ -3,13 +3,13 @@
 		Hello World
 	</div>
 	<div class="md:flex justify-between items-center max-w-8xl p-4 mx-auto border-t border-neutral-500 space-y-2 md:space-y-0">
-		<div class="footer-copyright">
+		<div class="footer-copyright text-center md:text-left">
 			&copy; <?php echo date_i18n( _x( 'Y', 'copyright date format', 'venus' ) ); ?>
 			<a class="uppercase" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 			<?php venus_technical_support(); ?>
 			<?php venus_icp(); ?>
 		</div>
-		<div class="space-x-4">
+		<div class="space-x-4 text-center md:text-right">
 			<a href="#" target="_blank" rel="nofollow"><i class="fab fa-telegram"></i></a>
 			<a href="#" target="_blank" rel="nofollow"><i class="fab fa-twitter"></i></a>
 			<a href="#" target="_blank" rel="nofollow"><i class="fab fa-facebook"></i></a>
