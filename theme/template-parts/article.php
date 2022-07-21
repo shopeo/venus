221c7142c257f95827978e96270900755ea46f7a
jabber <2898117012@qq.com>
2022-07-22 00:44:04 +0800

    init

--- theme/template-parts/article.php
+++ theme/template-parts/article.php
@@ -1,4 +1,5 @@
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class( 'max-w-5xl mx-auto p-4 md:pt-8' ); ?>
+		id="post-<?php the_ID(); ?>">
 	<?php
 	get_template_part( 'template-parts/featured-image' );
 	get_template_part( 'template-parts/entry-header' );
@@ -21,11 +22,16 @@
 		}
 		?>
 	</div>
+</article>
+<div class="max-w-5xl mx-auto p-4">
 	<?php
-	get_template_part( 'template-parts/navigation' );
 	if ( ( comments_open() || get_comments_number() ) && ! post_password_required() ) { ?>
 		<div class="comments-wrapper section-inner">
 			<?php comments_template(); ?>
 		</div>
-	<?php } ?>
-</article>
+	<?php }
+	?>
+</div>
+<div class="max-w-8xl mx-auto p-4 pb-8 md:py-8">
+	<?php get_template_part( 'template-parts/navigation' ); ?>
+</div>
