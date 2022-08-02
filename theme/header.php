0ac2543b92bf473a9f9c88210859af7c734a8bd0
jabber <2898117012@qq.com>
2022-08-03 03:01:52 +0800

    innit

--- theme/header.php
+++ theme/header.php
@@ -8,30 +8,35 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <header x-data="{ mobile_menu_open: false}" id="site-header" class="bg-white border-t-4 border-primary-500">
-	<div class="flex justify-between items-center max-w-8xl p-4 mx-auto space-x-14">
+	<div class="flex justify-between items-center max-w-8xl p-4 mx-auto">
 		<div class="flex-none">
 			<?php venus_site_logo(); ?>
 		</div>
 		<?php if ( has_nav_menu( 'primary' ) ) {
 			wp_nav_menu( array(
-					'container_class' => 'hidden md:block grow menu',
+					'container_class' => 'hidden md:block menu',
 					'menu_class'      => 'primary-menu flex space-x-10',
 					'theme_location'  => 'primary',
 					'depth'           => 2
 			) );
 		} ?>
-		<div class="hidden md:flex items-center space-x-4">
-			<?php $enable_header_search = get_theme_mod( 'enable_header_search', true );
-			if ( $enable_header_search === true ) {
-				get_search_form();
-			}
-			?>
-			<?php $enable_header_button = get_theme_mod( 'enable_header_button', true );
-			$header_button_text         = get_theme_mod( 'header_button_text' );
-			if ( $enable_header_button === true && $header_button_text ) {
-				venus_header_button();
-			} ?>
-		</div>
+		<?php
+		$enable_header_search = get_theme_mod( 'enable_header_search', true );
+		$enable_header_button = get_theme_mod( 'enable_header_button', true );
+		if ( $enable_header_search || $enable_header_button ) { ?>
+			<div class="hidden md:flex space-x-4">
+				<?php
+				if ( $enable_header_search === true ) {
+					get_search_form();
+				}
+				?>
+				<?php
+				$header_button_text = get_theme_mod( 'header_button_text', __( 'get started', 'venus' ) );
+				if ( $enable_header_button === true && $header_button_text ) {
+					venus_header_button();
+				} ?>
+			</div>
+		<?php } ?>
 		<div class="md:hidden">
 			<div @click="mobile_menu_open =! mobile_menu_open">
 				<span class="sr-only"><?php _e( 'Open menu', 'venus' ); ?></span>
@@ -49,17 +54,22 @@
 					'depth'           => 2
 			) );
 		} ?>
-		<div class="space-y-4">
-			<?php $enable_header_search = get_theme_mod( 'enable_header_search', true );
-			if ( $enable_header_search === true ) {
-				get_search_form();
-			}
-			?>
-			<?php $enable_header_button = get_theme_mod( 'enable_header_button', true );
-			$header_button_text         = get_theme_mod( 'header_button_text' );
-			if ( $enable_header_button === true && $header_button_text ) {
-				venus_header_button();
-			} ?>
-		</div>
+		<?php
+		$enable_header_search = get_theme_mod( 'enable_header_search', true );
+		$enable_header_button = get_theme_mod( 'enable_header_button', true );
+		if ( $enable_header_search || $enable_header_button ) { ?>
+			<div class="space-y-4">
+				<?php
+				if ( $enable_header_search === true ) {
+					get_search_form();
+				}
+				?>
+				<?php
+				$header_button_text = get_theme_mod( 'header_button_text', __( 'get started', 'venus' ) );
+				if ( $enable_header_button === true && $header_button_text ) {
+					venus_header_button();
+				} ?>
+			</div>
+		<?php } ?>
 	</div>
 </header>
