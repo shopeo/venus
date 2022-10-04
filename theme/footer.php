2f0a9680e5182fbbde87727390a21dcc4d05770f
jabber <2898117012@qq.com>
2022-10-04 22:12:29 +0800

    init

--- theme/footer.php
+++ theme/footer.php
@@ -4,7 +4,7 @@
 			<?php dynamic_sidebar( 'footer' ); ?>
 		</div>
 	</div>
-	<div class="md:flex justify-between items-center max-w-8xl p-4 mx-auto border-t border-neutral-300 space-y-2 md:space-y-0 text-neutral-500">
+	<div class="md:flex justify-between items-center max-w-8xl p-4 mx-auto border-t border-neutral-100 space-y-2 md:space-y-0 text-neutral-500">
 		<div class="footer-copyright text-center md:text-left">
 			&copy; <?php echo date_i18n( _x( 'Y', 'copyright date format', 'venus' ) ); ?>
 			<a class="uppercase" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
